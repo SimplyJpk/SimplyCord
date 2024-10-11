70159e825185431e7cd327c78e5afad6d1750aa4
James Kellie <james_kellie@live.com>
2024-10-12 01:01:05 +1000

    Update axios.ts

--- client/axios.ts
+++ client/axios.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = 'http://127.0.0.1:3000/api/v1';
 axios.defaults.headers.post['Content-Type'] = 'application/json';
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://localhost:5173';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = 'http://127.0.0.1:3000';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'http://127.0.0.1:3000/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-export default axiosInstance;
+export { axiosInstance };
