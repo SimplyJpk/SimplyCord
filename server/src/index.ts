3bc9a9ab249f16266935381fcfaa7376a0a24a18
James Kellie <james_kellie@live.com>
2024-11-05 20:35:35 +1000

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -42,7 +42,7 @@ if (!fs.existsSync(process.env.SERVER_DATA_PATH!)) {
 if (process.env.TEST_DOWNLOAD_MEDIA) {
   const downloadUrl = process.env.TEST_DOWNLOAD_MEDIA;
   const filePath = path.join(__dirname, '../temp.zip');
-  const outputDir = path.join(__dirname, '../dist/');
+  const outputDir = path.join(__dirname, '../');
 
   console.log('TEST_DOWNLOAD_MEDIA:', downloadUrl);
   console.log('TEST_DOWNLOAD_MEDIA_OUTPUT:', outputDir);
